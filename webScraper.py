25a0e20263ef43ea0a4ba37f290c6a7e631fb892
Jorge <ce.jorgecardona@gmail.com>
2018-05-03 23:14:02 -0500

    Some Minor Changes

--- webScraper.py
+++ webScraper.py
@@ -11,10 +11,13 @@ import urllib2                     # url requests
 import re                          # to get all images from url 
 import os                          # to test for a directory
 import sys                         # to parse from command line
+import subprocess
+from time import sleep
 from os.path import basename       
 from urlparse import urlsplit
 from urlparse import urlparse
 from posixpath import basename 
+from subprocess import call
 
 class ImageScrapper(object):
 
@@ -54,8 +57,10 @@ class ImageScrapper(object):
         self.DOMAIN = ''
         self.i =0
         self.flag = 0
+        self.isGood = 0
         self.url_len = len(str(url))
-
+        
+        # .com
         while(self.i < self.url_len):
             if(url[self.i] == '.'):
                 self.flag = 1
@@ -68,6 +73,7 @@ class ImageScrapper(object):
                             
             if(self.flag == 4):
                 self.DOMAIN += ".com"
+                self.isGood = 1
                 break
             else:
                 self.flag = 0
@@ -76,7 +82,43 @@ class ImageScrapper(object):
                 self.i += 1
         # End of while
 
-        return self.DOMAIN
+        if(self.flag == 4):
+            return self.DOMAIN
+        
+        # It was not .com
+        self.flag = 0
+        self.DOMAIN = ''
+        self.i =0
+        if not self.isGood:
+            # .org
+            while(self.i < self.url_len):
+                if(url[self.i] == '.'):
+                    self.flag = 1
+                    if(url[self.i+1] == 'o'):
+                        self.flag = 2
+                        if(url[self.i+2] == 'r'):
+                            self.flag = 3
+                            if(url[self.i+3] == 'g'):
+                                self.flag = 4
+                                
+                if(self.flag == 4):
+                    self.DOMAIN += ".org"
+                    self.isGood = 1
+                    break
+                else:
+                    self.flag = 0
+                
+                    self.DOMAIN += url[self.i]
+                    self.i += 1
+        # End of while
+
+    
+        if(self.flag == 4):
+            return self.DOMAIN
+
+        # It was not .org
+
+        return "UnknnownDomain"
     # End of getDomainName
 
 
@@ -84,7 +126,7 @@ class ImageScrapper(object):
         
         # Parsing the URL
         self.parse_object=urlparse(url)
-        print("\n\n parse_object: {}\n\n".format(self.parse_object));
+        print("\nparse_object: {}\n\n".format(self.parse_object));
 
         # Lets make a dir for our images:
         if not os.path.exists(ImageScrapper.IMG_DIR):
@@ -93,9 +135,22 @@ class ImageScrapper(object):
         # Lets make a request, and store the HTML locally
         self.urlRequest = urllib2.urlopen(url).read()
         self.source = str(DOMAIN)+'.html'
-        self.foo = open(self.source[8:], 'wb')
-        self.foo.write(self.urlRequest)
-        self.foo.close
+
+        if(self.source[:4] == 'https'):
+            self.foo = open(self.source[8:], 'wb')
+            self.foo.write(self.urlRequest)
+            self.foo.close
+
+        # HTTP
+        else:
+            try:
+                self.foo = open(self.source[7:], 'wb')
+                self.foo.write(self.urlRequest)
+                self.foo.close
+            except:
+                self.foo = open(self.source[8:], 'wb')
+                self.foo.write(self.urlRequest)
+                self.foo.close
 
         return self.urlRequest
     # End of fileUp
@@ -106,6 +161,19 @@ class ImageScrapper(object):
         
         # Lets use RE-X to find all the images in our urlRequested     
         self.imgurls = re.findall('img .*?src="(.*?)"',urlRequest)
+        print("Images try 1: {}".format(len(self.imgurls)))
+        
+        # They are trying to trick us...
+        if(len(self.imgurls) == 0):
+            self.imgurls = re.findall('img .*?alt="(.*?)"',urlRequest)
+            print("Images try 2: {}".format(len(self.imgurls)))
+        
+        if(len(self.imgurls) == 0):
+            self.imgurls = re.search('src="([^"]+)"',urlRequest)
+            #self.imgurls = re.findall('src=("*.jp*)"',urlRequest)
+            print("Images try 3: {}".format(len(self.imgurls)))
+
+        
         for self.imgurl in self.imgurls:
 
             # Lets try as is.
@@ -157,7 +225,7 @@ class ImageScrapper(object):
                     if(self.temp == self.IMG_DIR):
                         # THIS IS THE END??
                         return
-                    
+                     
                     self.output  = open(self.temp,'wb')
                     self.output.write(self.imgdata)
                     self.output.close()
@@ -165,8 +233,26 @@ class ImageScrapper(object):
                     
                     pass
 
+        print("\nDONE...\n")
     #End of getImage()
 
+
+    # Special Case:
+    def directURL(self, pic):
+
+        self.IMG_DIR = 'images/'
+        
+        self.imgurl = pic
+        print("Attempting as: {}".format(self.imgurl))
+        self.imgData = urllib2.urlopen(self.imgurl).read()
+        self.fileName = basename(urlsplit(self.imgurl)[2])
+        self.output = open(self.IMG_DIR+self.fileName,'wb')
+        self.output.write(self.imgData)
+        self.output.close()
+        print("Direct Link Download...Done\n")
+        
+
+        
 if __name__ == '__main__':
 
     RED = "\033[0;31m";
@@ -181,7 +267,15 @@ if __name__ == '__main__':
     pics = ImageScrapper();
 
     DOMAIN = pics.getDomainName(url)
+    print("\nDomain: {}".format(DOMAIN))
     requestURL = pics.fileUp(url, DOMAIN)
 
-    pics.getImages(url, requestURL, DOMAIN)
-    print("\n\nDONE...\n")
+    if(url[len(url)-3:] == 'jpg'):
+        pics.directURL(url)
+    elif(url[len(url)-3:] == 'png'):
+        pics.directURL(url)
+    else:
+        pics.getImages(url, requestURL, DOMAIN)
+
+   
+        
